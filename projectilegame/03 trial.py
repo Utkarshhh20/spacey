83bf7473a472269ee7c67980c3305bba89b89aea
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-10-18 23:53:27 +0400

    Update 03 trial.py

--- projectilegame/03 trial.py
+++ projectilegame/03 trial.py
@@ -7,13 +7,11 @@ from funcs.trajectory import get_trajectory, fig_from_list
 if "trayectory_list" not in st.session_state:
     st.session_state["trayectory_list"] = []
 
-# Title
 st.title("Trajectory of a projectile")
 st.subheader("Equations of motion of a projectile")
 st.latex("x(t) = v_0 \\cos(\\theta)t")
 st.latex("y(t) = v_0 \\sin(\\theta)t - \\frac{1}{2} g t^{2}")
 
-# Parameters
 st.subheader("Simulation parameters")
 c1, c2, c3 = st.columns(3)
 dv0 = 1
@@ -24,7 +22,7 @@ dtheta = 1
 theta_deg = c2.slider("Initial Angle [degrees]", 
                         min_value=5, max_value=90, 
                         value=45, step=5, help="Initial velocity for the projectile")
-# options for gravity: earth, moon, mars, jupiter
+
 gravity_dict = {'Earth': 9.8, 'Moon': 1.6, 'Mars': 3.7, 'Jupiter': 24.8}
 gravity_label = c3.selectbox("Gravity", gravity_dict.keys(), index=0)
 gravity = gravity_dict[gravity_label]
@@ -42,11 +40,13 @@ if len(st.session_state["trayectory_list"]) > 0:
     fig = fig_from_list(st.session_state["trayectory_list"])
     st.pyplot(fig)
 
-# The quizz
-st.subheader("Quizz time!")
+st.subheader("Quiz time!")
 
 stb.single_choice("At what angle is obtained the maximal distance?",
                 options=["15", "30", "45", "60", "75"], answer_index=2)
 
-stb.true_or_false("On the moon, the horizontal distance is always larger than on the earth under the same initial velocity and angle.",
+try:
+    stb.true_or_false("On the moon, the horizontal distance is always larger than on the earth under the same initial velocity and angle.",
                     answer=True)                
+except:
+    pass
