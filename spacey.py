d3a9257c38cfe86b4e4dcd67519e90a40c54b8df
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-10-18 22:04:22 +0400

    Create spacey.py

---
+++ spacey.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit_book as stb
+import base64
+import time
+import numpy as np
+import math
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="HappyBirds", page_icon="🐦")
+
+stb.set_book_config(path="HappyBirds")
