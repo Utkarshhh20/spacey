a56b73ac47e9c4fe2d25af37f0585005245663cb
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-10-19 16:02:27 +0400

    Update spacey.py

--- spacey.py
+++ spacey.py
@@ -306,8 +306,8 @@ elif dashboard=='SpaceY.':
     .centered-and-cropped {
       object-fit: cover;
       border-radius:50%;
-      width: 300px;
-      height: 300px; 
+      width: 230px;
+      height: 230px; 
     }
     </style>
     <body>
