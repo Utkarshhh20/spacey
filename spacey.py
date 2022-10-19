ef49131c6115769f438d95739698833a9f40afa0
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-10-19 17:00:31 +0400

    Update spacey.py

--- spacey.py
+++ spacey.py
@@ -263,6 +263,8 @@ if dashboard=='Create your own galaxy':
         .encode(x='x:Q', y='y:Q'))
     st.write('')
     st.subheader('How do galaxies form?')
+    st.write('In the early universe, there were no galaxies. Today, there are many billions. How did they form? Astronomers use the fundamental laws of physics to deduce the basic story of galaxy formation. Galaxies form out of immense clouds of gas that collapse and rotate. As they evolve, stars form within them. Entire galaxies can collide, changing their appearance. Looking deep into space, we see galaxies at earlier stages in their lives, and learn more about their evolution. They are more numerous, display unusual shapes, and have episodes of energetic outbursts. Galaxies can be seen back to more than 10 billion years ago.')
+    
     st.write('Galaxies form out of immense clouds of gas that collapse and rotate. As they evolve, stars form within them. Entire galaxies can collide, changing their appearance. Looking deep into space, we see galaxies at earlier stages in their lives, and learn more about their evolution.')
 elif dashboard=='Rocket Launch Prediction':
     st.title('Rocket Launch Prediction')
