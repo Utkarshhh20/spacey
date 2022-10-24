2ee878e5d6d20667f4f14b07e05f1620627735b0
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-10-24 20:38:58 +0400

    Update spacey.py

--- spacey.py
+++ spacey.py
@@ -603,7 +603,9 @@ elif dashboard=='SpaceY.':
     
     st.plotly_chart(fig, use_container_width=True)
     st.write('')
-    st.write('Our planetary system is located in an outer spiral arm of the Milky Way galaxy.\n Our solar system consists of our star, the Sun, and everything bound to it by gravity – the planets Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune; dwarf planets such as Pluto; dozens of moons; and millions of asteroids, comets, and meteoroids. Beyond our own solar system, we have discovered thousands of planetary systems orbiting other stars in the Milky Way.')
+    st.write('Our planetary system is located in an outer spiral arm of the Milky Way galaxy.')
+    st.write('Our solar system consists of our star, the Sun, and everything bound to it by gravity – the planets Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune; dwarf planets such as Pluto; dozens of moons; and millions of asteroids, comets, and meteoroids. Beyond our own solar system, we have discovered thousands of planetary systems orbiting other stars in the Milky Way.')
+    st.write('')
 if dashboard=='Space Quiz':
     n=0
     def ask_one_question(question):
